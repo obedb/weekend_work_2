2dc00b61bd6d997b4c6b15f708fbba05eac7bde6
Obed Bigirimana <obedbigirimana@gmail.com>
2017-01-22 11:26:51 +0200

    I have completed the first exercise about algo

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,6 +1,7 @@
 # Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
 
-def palindrome?(string)
+def palindrome?(result)
+  result == result.reverse
 
 end
 
--- algo/splitting_arrays.rb
+++ algo/splitting_arrays.rb
@@ -1,6 +1,14 @@
 # Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
 
 def split_array(array, array_size)
+  @array = array
+  @array_size = array_size
+
+  combine_array = [array, array_size]
+  final_combine_arrays = combine_array.flatten.uniq.sort 
+  result = final_combine_arrays.each_slice(array_size).to_a 
+
+
 
 end
 
